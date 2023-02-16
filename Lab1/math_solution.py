4520898601c973ccbb78e75dbb90d6fb23b4636a
Dmirtiy Busygin <Busygin.voll@mail.ru>
2023-02-16 14:17:06 +0300

    fix 1st root zero exception

--- Lab1/math_solution.py
+++ Lab1/math_solution.py
@@ -47,7 +47,7 @@ def gauss_method_solution(matrix_list):
         print("Шаг №", i + 1, ":")
         print_matrix(matrix, f)
     x = [0] * n
-    for i in range(n - 1, 0, -1):
+    for i in range(n - 1, -1, -1):
         s = 0
         for j in range(i + 1, n):
             s = s + matrix[i][j] * x[j]
